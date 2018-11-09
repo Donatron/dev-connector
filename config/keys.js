a9f1a554e8b70681308f5b0922104fc6966eda43
Donatron <don.macarthur68@gmail.com>
2018-11-10 07:56:02 +1000

    added user regiter and login routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://Don:iamd0n@ds157493.mlab.com:57493/devconnector"
+  mongoURI: "mongodb://Don:iamd0n@ds157493.mlab.com:57493/devconnector",
+  secretOrKey: "secret"
 };
