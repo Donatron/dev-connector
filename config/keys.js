f53eeb7c6776876930e5ef462ea440a4e9e074e6
Donatron <don.macarthur68@gmail.com>
2018-12-31 13:41:17 +1000

    prepared for deployment

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://Don:iamd0n@ds157493.mlab.com:57493/devconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
